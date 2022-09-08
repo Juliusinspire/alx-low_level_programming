59d3a78c43d9e2ddf24defff7c78670cbac011e5
Juliusinspire <juliusortega2000@gmail.com>
2022-09-08 15:47:31 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- serve as the entry point of the program
+ *
+ * return: By default returns zero if not error otherwise returns non-zero value.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
