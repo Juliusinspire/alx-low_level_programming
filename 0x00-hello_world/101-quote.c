2873830ab981373617596d49800ae9dbabe21db2
Juliusinspire <juliusortega2000@gmail.com>
2022-09-08 21:56:59 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Print a text
+ *
+ * Return: sucess
+ */
+int main(void)
+{
+	write (2, "and that piece of art is useful\" - Dors korpar, 2015-10-19\n", 59)
+        return (1)
+}
