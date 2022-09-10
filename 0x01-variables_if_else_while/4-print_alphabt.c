38884b8e9af2e2608c68fd0f3ac10aff56771bb7
Juliusinspire <juliusortega2000@gmail.com>
2022-09-10 05:27:49 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	f (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+	putchar('\n');
+	return (0);
+}
