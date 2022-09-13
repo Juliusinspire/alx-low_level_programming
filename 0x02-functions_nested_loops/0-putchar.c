68dedb5cea034fe327ae4f9c6b6199d83aafaf00
Juliusinspire <juliusortega2000@gmail.com>
2022-09-13 16:56:54 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-	putchar(");
+	putchar('');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('c);
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
