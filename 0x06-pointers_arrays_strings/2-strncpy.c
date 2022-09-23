45e84e0c92ac5cc223c04227ef653bc5604b105d
Juliusinspire <juliusortega2000@gmail.com>
2022-09-23 16:51:25 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,69 @@
-#include "main.h"
-/**
- * _strncpy - C function that copies a string, including the
- * terminating null byte, using at most an inputted number of bytes.
- * If the length of the source string is less than the maximum byte number,
- * the remainder of the destination string is filled with null bytes.
- * works identically to the standard library function 'strncpy'
- * @dest: buffer storing the string copy
- * @src: the source of the string
- * @n: max number of the byte copied
- * Return: returns
- */
+trncpy - A function that copies a string.
 
-char *_strncpy(char *dest, char *src, int n)
+ *
+ *
+ *  * @dest: pointer to destination input buffer
+ *
+ *   * @src: pointer to source input buffer
+ *
+ *    * @n: bytes of @src
+ *
+ *     *
+ *
+ *      * Return: @dest
+ *
+ *      */
+
+
+
+ char *_strncpy(char *dest, char *src, int n)
+
 {
-	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+		int i;
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
 
-		return (dest);
+
+			/**
+			 *
+			 * 	 * iterate through src array
+			 *
+			 * 	 	 * where if there is no null byte
+			 *
+			 * 	 	 	 * among the first n bytes of source
+			 *
+			 * 	 	 	 	 * the string placed in dest will not be
+			 *
+			 * 	 	 	 	 	 * null terminated
+			 *
+			 * 	 	 	 	 	 	*/
+
+			for (i = 0; i < n && src[i] != '\0'; i++)
+
+						dest[i] = src[i];
+
+				/**
+				 *
+				 * 	 * if the length of source is less than n
+				 *
+				 * 	 	 * write additional nullbytes to dest to
+				 *
+				 * 	 	 	 * ensure that a total of n bytes is written
+				 *
+				 * 	 	 	 	*/
+
+				while (i < n)
+
+						{
+
+									dest[i] = '\0';
+
+											i++;
+
+												}
+
+
+
+					return (dest);
+
 }
