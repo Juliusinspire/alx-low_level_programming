0355bdd7167d9bf2d64ce8549718648439ba1cba
Juliusinspire <juliusortega2000@gmail.com>
2022-09-30 02:41:45 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point to program
+ *
+ * @argc: commad line argc count
+ *
+ * @argv: arrays of command line argv
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (argc--)
+			printf("%s\n", argv[i++]);
+	return (0);
+}
+
