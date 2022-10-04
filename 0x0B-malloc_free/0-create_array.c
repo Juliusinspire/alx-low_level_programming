a6bc8e29fd13436cc5ef33e97f23fe6d7bbdde51
Juliusinspire <juliusortega2000@gmail.com>
2022-10-04 12:17:32 -0700

    Task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * create_array - ghhfgh
+ * @size: dff
+ * @c: character
+ * Return: ptr
+ */
+char *create_array(unsigned int size, char c)
+{
+		size_t i = 0;
+			char *ptr = (char *) malloc((sizeof(char) * size));
+
+				if (size == 0)
+							return (NULL);
+					if (!ptr)
+								return (NULL);
+						while (i < size)
+								{
+											*(ptr + i) = c;
+													i++;
+														}
+							return (ptr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "stdlib.h"
+#include "string.h"
+/**
+ * _strdup - strdup
+ *  @str: str
+ * Return: dest
+ */
+char *_strdup(char *str)
+{
+		int i = 0;
+			int len;
+				char *dest;
+
+					if (str == NULL)
+								return (NULL);
+						len = strlen(str) + 1;
+							dest = (char *) malloc((len * sizeof(char)));
+								if (!dest)
+											return (NULL);
+									while (i < len)
+											{
+														*(dest + i) = *(str + i);
+																i++;
+																	}
+										return (dest);
+}
