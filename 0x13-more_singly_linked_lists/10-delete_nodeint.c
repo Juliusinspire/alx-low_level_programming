0df6284bf865525b5ca5a0049b1230488a39b23f
Juliusinspire <juliusortega2000@gmail.com>
2022-11-15 14:06:15 -0800

    Task 100

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,52 +1,40 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- *  *  * delete_nodeint_at_index - A function that deletes the node at
- *   *
- *    *   * specified index from list
- *     *
- *      *    * @head: A pointer to listint_t structure
- *       *
- *        *     * @index: The index of the node to delete
- *         *
- *          *      * Return: 1 if succeeded, or -1 if it failed
- *           *
- *            */
-
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 (Success), or -1 (Fail)
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-			listint_t *temp, *dnode;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-						unsigned int i = 1;
+	if (*head == NULL)
+		return (-1);
 
-									if (*head == NULL)
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
 
-														return (-1);
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
 
-												temp = *head;
 
-															if (index == 0)
-																			{
-																							*head = (*head)->next;
-																										free(temp);
-																													}
-																		else
-																						{
-																										while (i < index)
-																															{
-																																					temp = temp->next;
-																																										if (temp == NULL)
-																																																	return (-1);
-																																															i++;
-																																																			}
-																														dnode = temp;
-																																		dnode = dnode->next;
-																																						temp->next = dnode->next;
-																																										free(dnode);
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
 
-																																												}
-
-																				return (1);
+	return (1);
 }
--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,29 +1,26 @@
+
 #include "lists.h"
 
 /**
- *  * reverse_listint - A function that reverses a linked list
- *   *
- *    *   * @head: A pointer to listint_t structure
- *     *
- *      *    * Return: A pointer to the first node of the reverse list
- *       */
-
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-		listint_t *prev = NULL, *next;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-			if (*head == NULL)
-						return (NULL);
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
 
-				while (*head)
-						{
-									next = (*head)->next;
-											(*head)->next = prev;
-													prev = *head;
-															*head = next;
-																}
-					*head = prev;
+	*head = prev;
 
-						return (*head);
-
+	return (*head);
 }
